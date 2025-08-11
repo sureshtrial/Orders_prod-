510f472d42da1a8b17315790a3ae14e21bca7a84
Suresh V <suresh.v2008@gmail.com>
2025-08-11 16:05:21 +0530

    scd-2 snapshots2

--- snapshots/snap_dim_customer.sql
+++ snapshots/snap_dim_customer.sql
@@ -1,8 +1,9 @@
+{% snapshot snap_dim_customer %}
 {{ config(
     target_schema='snapshots',
     unique_key='customer_id',
     strategy='check',
-    check_cols=['first_name', 'last_name', 'email']
+    check_cols=['first_name', 'last_name', 'email', 'signup_date']
 ) }}
 
 SELECT
@@ -12,3 +13,5 @@ SELECT
     email,
     signup_date::date AS signup_date
 FROM {{ source('raw', 'customers') }}
+
+{% endsnapshot %}
